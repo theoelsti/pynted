44d977657470c01740c853a2dffd47b3b8f71049
theoelsti <theophile.fouillet@protonmail.com>
2024-01-13 16:58:35 +0100

    Add new exceptions for user, filter, and brand not found

--- pynted/exceptions/vinted.py
+++ pynted/exceptions/vinted.py
@@ -3,3 +3,16 @@ class UserNotFound(Exception):
     def __init__(self, message: str = "User not found", username: str = None):
         super().__init__(message)
         self.username = username
+
+
+class NoFilterRetrieved(Exception):
+    """Raised when no filter is retrieved"""
+    def __init__(self, message: str = "No filter retrieved"):
+        super().__init__(message)
+
+
+class BrandNotFound(Exception):
+    """Raised when the brand is not found"""
+    def __init__(self, message: str = "Brand not found", brand: str = None):
+        super().__init__(message)
+        self.brand = brand

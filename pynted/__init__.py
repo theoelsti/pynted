d04efc2207e83ab1f32702ce9367f3897fcab659
theoelsti <theophile.fouillet@protonmail.com>
2024-01-13 16:57:28 +0100

    Add pynted library documentation

--- pynted/__init__.py
+++ pynted/__init__.py
@@ -1,1 +1,26 @@
+"""
+Vinted python library
+~~~~~~~~~~~~~~~~~~~~~
+
+Pynted is an vinted library, written in Python.
+Basic usage:
+
+   >>> import pynted
+   >>> vinted = pynted.Pynted()
+   >>> vinted.get_user_by_username("foo")
+    <VintedUser foo>
+
+
+Differents exceptions are raised when an error occured.
+You can catch them like this:
+
+   >>> import pynted
+   >>> vinted = pynted.Pynted()
+   >>> try:
+   ...     vinted.get_user_by_username("bar")
+   ... except pynted.exceptions.vinted.UserNotFound as e:
+   ...     logger.error("User %s not found", e.username)
+   User bar not found
+
+"""
 __all__ = []
